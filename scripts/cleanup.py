cafd67e32e3403e2159d9537d85a4ecd708f573a
Hans Bacamante <bacamantepher@gmail.com>
2025-09-26 22:45:20 +0800

    First Commit

---
+++ scripts/cleanup.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+#Import Data
+df = pd.read_csv(r"C:\Users\Hans Christopher\Documents\DATA ANALYST TOOLS\PYTHON\Sales\Original Data\realistic_linear_regression_dataset.csv")
+
+#Cleaner
+print(df.info())
